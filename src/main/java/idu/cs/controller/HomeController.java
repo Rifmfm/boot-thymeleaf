c181fadc2e99515fb4fd9ddd771ae698cd0e4a1e
Euna <2752657@gmail.com>
2019-06-18 23:43:51 +0900

    user기능 정리(로그인, 수정, 리스트 등)

---
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -0,0 +1,66 @@
+package idu.cs.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import idu.cs.domain.User;
+import idu.cs.entity.UserEntity;
+import idu.cs.service.QuestionService;
+import idu.cs.service.UserService;
+
+@Controller
+public class HomeController {
+	@Autowired UserService userService;
+	@Autowired QuestionService questionService;
+	
+	/*
+	 * return "A" = A에 해당하는 view를 보여주는 것
+	 * return "redirect:/A" = A주소로 URL 요청을 다시 하는 것
+	 */
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+
+	@GetMapping("/users/login-form")
+	public String getLoginForm(Model model) {
+		return "/users/login";
+	}
+	
+	@PostMapping("/login")
+	public String loginUser(@Valid UserEntity user, HttpSession session) { // 로그인할때 여기를 지나감
+		System.out.println(user.getUserId() + "님 login 시도");
+		User sessionUser = userService.getUserByUserId(user.getUserId());
+		if(sessionUser == null) {
+			System.out.println("id error");
+			return "redirect:/users/login-form";
+		}
+		if(!sessionUser.getUserPw().equals(user.getUserPw())) {
+			System.out.println(("pw error"));
+			return "redirct:/users/login-form";
+		}
+		session.setAttribute("user", sessionUser);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String logoutUser(HttpSession session) {
+		//session.removeAttribute("user");
+		session.invalidate();  // 세션을 소멸
+		return "redirect:/";
+	}
+	
+	@GetMapping("/users/register-form")
+	public String getRegForm(Model model) {
+		return "/users/register";
+	}
+	
+	
+}
--- src/main/java/idu/cs/controller/UserController.java
+++ src/main/java/idu/cs/controller/UserController.java
@@ -1,11 +1,8 @@
 package idu.cs.controller;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,134 +10,53 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import idu.cs.domain.User;
-import idu.cs.entity.UserEntity;
 import idu.cs.exception.ResourceNotFoundException;
-import idu.cs.repository.UserRepository;
 import idu.cs.service.UserService;
 
-@Controller  // Spring Framework에게 이 클래스로 부터 작성된 객체는 Controller 역할을 함을 알려줌  Spring이 이 클래스로부터 Bean 객체를 생성해서 등록할 수 있음
+@Controller
+@RequestMapping("/users")
+//@Component, @Controller, @Repository, @Service 표시된 클래스형 빈 객체를 스프링이 스캔하여 등록하고, @Autowired 등 요청시 주입 	
 public class UserController {
-	@Autowired UserService userService;	// 의존성 주입 : UserRepository를 userRepo에 Autowired해줘
+	@Autowired UserService userService;	// 의존성 주입
 	
-	// 전체 돌아가는 상황을 알자!
-	// 뭘 누르면 뭐로 가고 그래서 뭐가 나오는지? 돌아가는 느낌 적인 느낌 느낌!
-	
-	@GetMapping("/")
-	public String home(Model model) {
-		return "index";
-	}
-	@GetMapping("/user-login-form")
-	public String getLoginForm(Model model) {
-		return "login";
+	@GetMapping("")  // list
+	public String getUsers(Model model, HttpSession session) {
+		model.addAttribute("users", userService.getUsers());
+		return "/users/list";
 	}
 
-	@PostMapping("/login")
-	public String loginUser(@Valid UserEntity user, HttpSession session) { // 로그인할때 여기를 지나감
-		System.out.println("님 login 성공");
-		User sessionUser = userService.getUserByUserId(user.getUserId());
-		if(sessionUser == null) {
-			System.out.println("id error");
-			return "redirect:/user-login-form";
-		}
-		if(!sessionUser.getUserPw().equals(user.getUserPw())) {
-			System.out.println(("pw error"));
-			return "redirct:/user-login-form";
-		}
-		session.setAttribute("user", sessionUser);
-		return "redirect:/";
+	@PostMapping("")  // register, create
+	public String createUser(@Valid User formUser, Model model) {  // @valid : 도메인 클래스에서 지정한 규칙에 어긋날 경우 돌려보냄
+		userService.saveUser(formUser);
+		model.addAttribute("user", formUser);
+		return "redirect:/users";  // get 방식으로 redirect = 즉, list로 간다는 이야기
 	}
-	
-	@GetMapping("/logout")
-	public String logoutUser(HttpSession session) {
-		session.removeAttribute("user");
-		// session.invalidate();
-		return "redirect:/";
-	}
-	@GetMapping("/user-register-form")
-	public String getRegForm(Model model) {
-		return "register";
-	}
-	@GetMapping("/user-update-form")
-	public String getupdateForm(Model model, HttpSession session) {
-		// 서비스를 통해 리파지터리로 부터 정보를 가져와야 하나 세션에 저장해두었으므로 활용함
-		User user = (User) session.getAttribute("user");
+
+	@GetMapping("/{id}")  // update_form
+	public String getupdateForm(@PathVariable(value = "id") Long id, Model model) {
+		User user = userService.getUserById(id);
 		model.addAttribute("user", user);
-		/*User sessionUser = userService.getUserById(user.getId());
-		model.addAttribute("user", sessionUser);*/
-		return "info";
+		return "/users/info";
 	}
 	
-	@PutMapping("/users/{id}")
-	public String updateUser(@PathVariable(value = "id") Long id, @Valid User user, Model model, HttpSession session) { 
-		// updtaeUser 객체는 입력 폼의 내용 - id값이 없음
-		user.setId(userService.getUserById(id).getId());
+	@PutMapping("/{id}")  // update
+	public String updateUser(@PathVariable(value = "id") Long id, @Valid User formUser, Model model, HttpSession session) { 
+		User user = userService.getUserById(id);
+		user.setUserPw(formUser.getUserPw());
+		user.setName(formUser.getName());
+		user.setCompany(formUser.getCompany());
 		userService.updateUser(user);
 		session.setAttribute("user", user);
 		return "redirect:/users";
 	}
-
-	// 이러한 방식이 더 좋아서 바꾸느라고 다 주석처리 해버림
-	@GetMapping("/users")
-	public String getAllUser(Model model, HttpSession session) {
-		model.addAttribute("users", userService.getUsers());
-		return "userlist";
-	}
-	/*
-	@GetMapping("/users")
-	public String getAllUser(Model model) {
-		model.addAttribute("users", userService.getUsers());
-		return "/users/list";
-	}	*/
 	
-	@PostMapping("/users")
-	public String createUser(@Valid User user, Model model) {  // User 앞 @RequestBody 제거
-		userService.saveUser(user);
-		return "redirect:/users";  // get 방식으로 해당 url로  재지정(redirect)
+	@DeleteMapping("/{id}")  // delete
+	public String deleteUser(@PathVariable(value = "id") Long userId, @Valid User formUser, Model model) {
+		userService.deleteUser(formUser);
+		model.addAttribute("name", formUser.getName());
+		return "/users/delete";
 	}
-	/*
-	@GetMapping("/users/{id}")  // 오류 해결방법 찾기 : ctrl + 1
-	public String getUserById(@PathVariable(value = "id") Long userId, Model model) throws ResourceNotFoundException {
-		UserEntity user = userRepo.findById(userId).get(); 
-				//orElseThrow는 에러처리를 내가 지정 가능<-.orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
-		//() -> new Resource..." + userId) = 람다식 (함수형 인터페이스)
-		//model.addAttribute("id", user.getId());  // user.getID = userId  아이디를 넘김
-		model.addAttribute("user", user);  // 하나의 객체로 넘기기
-		return "user";
-		//return ResponseEntity.ok().body(user);
-	}  
-	@GetMapping("/users/fn")
-	public String getUserByName(@Param(value="name") String name, Model model) {
-		List<UserEntity> users = userRepo.findByName(name);
-		model.addAttribute("users",users);
-		return "userlist";
-	}
-	
-	/* 
-	 * 메소드 방식에 따라 다르게 동작 시킬 것이다. - Restfull방식
-	 * 자원 중심 : 자원의 현상태를 전송하자  ex) get방식, put방식에 따라 다르게 동작
-	 * 중요한 이유 : 이게 이름?(url?)을 다르게 해서 쓰면 update를 썼나? upload를 썼나? 헷갈릴 수 있다.
-	 *//*
-	@PutMapping("/users/{id}")  // @PatchMapping : 수정된 것만 고치도록 하는 친구
-	public String updateUser(@PathVariable(value = "id") Long userId, @Valid UserEntity userDetails, Model model, HttpSession session) { 
-		UserEntity user = userRepo.findById(userId).get();  // user는 DB로 부터 읽어온 객체
-		user.setUserId(userDetails.getUserId());
-		user.setUserPw(userDetails.getUserPw());
-		user.setName(userDetails.getName());  // userDetails는 전송한 객체
-		user.setCompany(userDetails.getCompany());
-		userRepo.save(user);
-
-		return "redirect:/users";
-	}
-	
-	@DeleteMapping("/users/{id}")
-	public String deleteUser(@PathVariable(value = "id") Long userId, Model model) {
-	// @PathVariable(value = "id") Long userId = id를 가져오기
-		UserEntity user = userRepo.findById(userId).get();
-		userRepo.delete(user);
-		model.addAttribute("name", user.getName());
-		return "user-deleted";
-	}*/
 }
